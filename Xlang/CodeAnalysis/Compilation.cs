a8e5482b666b67757a80a861fb9c2687ce9ad106
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2018-10-31 21:05:48 +0100

    Use ImmutableArray<T>

--- Xlang/CodeAnalysis/Compilation.cs
+++ Xlang/CodeAnalysis/Compilation.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using Xlang.CodeAnalysis.Binding;
 using Xlang.CodeAnalysis.Syntax;
@@ -20,14 +21,14 @@ namespace Xlang.CodeAnalysis
             var binder = new Binder(variables);
             var boundExpression = binder.BindExpression(Syntax.Root);
 
-            var diagnostics = Syntax.Diagnostics.Concat(binder.Diagnostics).ToArray();
+            var diagnostics = Syntax.Diagnostics.Concat(binder.Diagnostics).ToImmutableArray();
             if (diagnostics.Any())
                 return new EvaluationResult(diagnostics, null);
 
             var evaluator = new Evaluator(boundExpression, variables);
             var value = evaluator.Evaluate();
 
-            return new EvaluationResult(Array.Empty<Diagnostic>(), value);
+            return new EvaluationResult(ImmutableArray<Diagnostic>.Empty, value);
         }
     }
 }
